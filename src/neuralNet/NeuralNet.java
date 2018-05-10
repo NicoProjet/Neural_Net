37014f52690b1810cb8749611c9db0e35ba5644c
Max <dewit.maxime28@gmail.com>
2018-05-10 10:58:13 +0200

    Init

--- src/neuralNet/NeuralNet.java
+++ src/neuralNet/NeuralNet.java
@@ -1,5 +1,24 @@
 package neuralNet;
 
-class NeuralNet {
+import java.util.*;
 
+
+class NeuralNet {
+	
+	int nLayers;
+	ArrayList<Integer> size;
+	ArrayList<Integer> biases;
+	ArrayList<Integer> weights;
+	
+	
+
+	public NeuralNet(ArrayList<Integer> givenSize){
+		nLayers = givenSize.size();
+		size = givenSize;
+		
+		
+		
+	}
+	
+	
 }
