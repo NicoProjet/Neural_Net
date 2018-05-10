3c41f33732f30bc899cf238b9e9c911a883bcfeb
NicoProjet <nicolas.feron@ulb.ac.be>
2018-05-10 11:44:24 +0200

    finished import

--- src/neuralNet/Main.java
+++ src/neuralNet/Main.java
@@ -3,12 +3,22 @@ package neuralNet;
 import java.io.IOException;
 
 public class Main {
-	static int size = 50;
 	public static void main(String[] args) throws IOException {
 		/* read labels
-        byte[] data = ReadMNIST.readLabels(size);
-        for (int j=8; j<size; j++){
+        int[] data = ReadMNIST.readLabels(50);
+        for (int j=0; j<data.length; j++){
     		System.out.print(data[j]+"\n");
+    	}
+    	*/
+    	
+		/* read images
+		int[] data = ReadMNIST.readImages(1);
+        for (int i=0; i<28; i++){
+        	for (int j=0; j<28; j++){
+        		String value = String.format("%4d", data[i*28+j]);
+        		System.out.print(value);
+        	}
+        	System.out.print("\n");
     	}
     	*/
 	}
--- src/neuralNet/ReadMNIST.java
+++ src/neuralNet/ReadMNIST.java
@@ -3,21 +3,34 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import org.apache.commons.io.IOUtils;
 
 class ReadMNIST {
-	/*
-	public static ArrayList<Byte> readLabels(){
-		ArrayList<Byte> labels = new ArrayList<Byte>();
-		return labels;
-	}
-	*/
+	public static int imageSize = 28;
 	
-	public static byte[] readLabels(int numberOfLabels) throws IOException{
+	private static int[] byteArrayTointArray(byte[] array, int headerSize){
+		int[] intArray = new int[array.length];
+		for (int i=0; i<array.length-headerSize; i++){
+			intArray[i] = array[i+headerSize] & 0xff;
+		}
+		return intArray;
+	}
+	
+	public static int[] readLabels(int numberOfLabels) throws IOException{
 		File MNIST_labels = new File("data/train-labels.idx1-ubyte");
 		InputStream fileStream = new FileInputStream(MNIST_labels);
-		return IOUtils.toByteArray(fileStream, numberOfLabels);
+		byte[] byteArray = IOUtils.toByteArray(fileStream, numberOfLabels);
+		int headerSize = 8;
+		return byteArrayTointArray(byteArray, headerSize);
+	}
+	
+	public static int[] readImages(int numberOfImages) throws IOException{
+		File MNIST_images = new File("data/train-images.idx3-ubyte");
+		InputStream fileStream = new FileInputStream(MNIST_images);
+		int headerSize = 16;
+		int bytesToGet = numberOfImages * imageSize * imageSize + headerSize;
+		byte[] byteArray =  IOUtils.toByteArray(fileStream, bytesToGet);
+		return byteArrayTointArray(byteArray, headerSize);
 	}
 
 }
